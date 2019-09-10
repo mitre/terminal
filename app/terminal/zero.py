1016780038d4db9b8adffbe1bf93e6d7229c4d56
david <davidhunt@mitre.org>
2019-09-10 10:34:23 -0400

    adding new exfil option to terminalg

--- app/terminal/zero.py
+++ app/terminal/zero.py
@@ -27,6 +27,8 @@ class Zero:
     async def _help():
         print('ZERO SHELL HELP:')
         print('-> bg: send this shell to the background')
+        print('-> download [filename]: drop any file from the API')
+        print('-> upload [filename]: exfil any file or archive to the API')
 
     async def _execute(self, cmd):
         self.conn.send(str.encode('%s\n' % cmd))
