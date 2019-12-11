51695205743b7a463ea3aaa1594b4253a1826120
Henry Foster <hfoster@mitre.org>
2019-12-11 11:33:44 -0500

    consolidate shell initialization into _handshake method

--- app/utility/session.py
+++ app/utility/session.py
@@ -3,6 +3,10 @@ import socket
 from plugins.terminal.app.utility.console import Console
 
 
+class ShellHandshakeFailure(ConnectionError):
+    pass
+
+
 class Session:
 
     def __init__(self, services):
@@ -13,10 +17,11 @@ class Session:
         self.seen_ips = set()
 
     async def accept(self, reader, writer):
-        if not (await self._handshake(reader, writer)):
+        try:
+            shell_info = await self._handshake(reader, writer)
+        except ShellHandshakeFailure:
             return
         connection = writer.get_extra_info('socket')
-        shell_info = await self._get_shell_info(reader)
         self.sessions.append(dict(id=len(self.sessions) + 1, paw=shell_info, connection=connection))
         self.console.line('New session: %s' % shell_info)
 
@@ -35,18 +40,14 @@ class Session:
         recv_proof = (await reader.readline()).strip()
         remote_socket = writer.get_extra_info('socket').getpeername()
         if recv_proof.decode() in self.term_svc.terminal_keys:
-            return True
+            return (await reader.readline()).strip().decode()
         elif remote_socket[0] in self.seen_ips:  # already seen, don't re-notify that this is blocked
             writer.close()
-            return False
+            raise ShellHandshakeFailure
         else:
             self.console.line(
                 'Blocked an incoming connection from {} with incorrect terminal_key value {}\n'.format(remote_socket,
                                                                                                        recv_proof))
             self.seen_ips.add(remote_socket[0])
             writer.close()
-            return False
-
-    async def _get_shell_info(self, reader):
-        x = (await reader.readline()).strip().decode()
-        return x
+            raise ShellHandshakeFailure
