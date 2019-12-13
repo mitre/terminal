47ae8b32ad16f1fe3dc21280deb2ccb1379a87c8
Hunt, David <davidhunt@mitre.org>
2019-12-13 09:10:42 -0500

    adjusting

--- app/terminal/zero2.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Welcome to the Zero shell')
-    parser.add_argument('-C', '--connection')
-    args = parser.parse_args()
