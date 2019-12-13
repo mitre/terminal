57785ab355d0eeba78498bdcbcb125a829c5dbe8
Hunt, David <davidhunt@mitre.org>
2019-12-13 15:52:26 -0500

    adjusting terminal to make it slim

--- app/term_svc.py
+++ app/term_svc.py
@@ -3,12 +3,20 @@ import string
 
 from shutil import which
 
+from aiohttp_jinja2 import template
+
 
 class TermService:
 
-    def __init__(self, file_svc, terminal_keys):
-        self.file_svc = file_svc
+    def __init__(self, services, terminal_keys):
+        self.file_svc = services.get('file_svc')
+        self.auth_svc = services.get('auth_svc')
         self.terminal_keys = terminal_keys
+
+    @template('terminal.html')
+    async def splash(self, request):
+        await self.auth_svc.check_permissions(request)
+        return dict(sessions=[1,1,2,2,3,4])
 
     async def dynamically_compile(self, headers):
         name, platform = headers.get('file'), headers.get('platform')
@@ -20,7 +28,7 @@ class TermService:
             output = 'plugins/%s/payloads/%s-%s' % (plugin, name, platform)
             self.file_svc.log.debug('Dynamically compiling %s' % name)
             await self.file_svc.compile_go(platform, output, file_path, ldflags=' '.join(ldflags))
-        return '%s-%s' % (name, platform), '%s-%s' % (name, platform)
+        return '%s-%s' % (name, platform)
 
     """ PRIVATE """
 
