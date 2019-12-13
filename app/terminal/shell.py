96b050421d825dd5e669dd81a4f687f2e0218d78
Hunt, David <davidhunt@mitre.org>
2019-12-13 15:52:26 -0500

    adjusting terminal to make it slim
    Former-commit-id: 57785ab355d0eeba78498bdcbcb125a829c5dbe8

--- app/terminal/shell.py
+++ app/terminal/shell.py
@@ -2,7 +2,6 @@ import asyncio
 
 from aioconsole import ainput
 
-from app.objects.c_operation import Operation
 from plugins.terminal.app.terminal.zero import Zero
 from plugins.terminal.app.utility.console import Console
 from plugins.terminal.app.utility.session import Session
@@ -11,8 +10,6 @@ from plugins.terminal.app.utility.session import Session
 class Shell:
 
     def __init__(self, services):
-        self.data_svc = services.get('data_svc')
-        self.planning_svc = services.get('planning_svc')
         self.app_svc = services.get('app_svc')
         self.session = Session(services)
         self.prompt = 'caldera> '
@@ -28,10 +25,8 @@ class Shell:
                     await self.session.refresh()
                     commands = {
                         'help': lambda _: self._help(),
-                        'agents': lambda _: self._show_agents(),
                         'sessions': lambda _: self._show_sessions(),
                         'join': lambda c: self._connect_session(cmd),
-                        'new': lambda c: self._new_session(cmd)
                     }
                     command = [c for c in commands.keys() if cmd.startswith(c)]
                     await commands[command[0]](cmd)
@@ -43,21 +38,9 @@ class Shell:
     @staticmethod
     async def _help():
         print('HELP MENU:')
-        print('-> agents: show all connected computers')
+        print('Start a session by creating a new operation with the terminal adversary')
         print('-> sessions: show all active sessions')
-        print('-> new [n]: start a new session on an agent idx')
         print('-> join [n]: connect to a session by ID')
-
-    async def _sorted_agent_list(self):
-        return sorted(await self.data_svc.locate('agents'), key=lambda a: a.last_seen)
-
-    async def _show_agents(self):
-        hosts = []
-        for i, a in enumerate(await self._sorted_agent_list()):
-            hosts.append(dict(idx=str(i), host=a.host, user=a.username, priv=a.privilege, paw=a.paw, trusted=a.trusted))
-        if not hosts:
-            self.console.hint('Deploy 54ndc47 agents to add new hosts')
-        await self.console.table(hosts)
 
     async def _show_sessions(self):
         await self.console.table([dict(idx=s['id'], paw=s['paw']) for s in self.session.sessions])
@@ -71,21 +54,3 @@ class Shell:
             await Zero(conn).enter()
         except StopIteration:
             self.console.line('Session cannot be established with %s' % session, 'red')
-
-    async def _new_session(self, command):
-        agent_idx = int(command.split(' ')[1])
-        agent = (await self._sorted_agent_list())[agent_idx]
-        if agent:
-            adv = await self.data_svc.locate('adversaries', match=dict(adversary_id='56aebecf-abca-40c1-ad24-658e7c25b55b'))
-            planner = await self.data_svc.locate('planners')
-            agents = await self.data_svc.locate('agents', match=dict(group=agent.group))
-            op = await self.data_svc.store(
-                Operation(id='1000', name='terminal', adversary=adv[0], agents=agents, planner=planner[0],
-                          state='running')
-            )
-            op.set_start_details()
-            loop = asyncio.get_event_loop()
-            loop.create_task(self.app_svc.run_operation(op))
-            self.console.line('Queued. Waiting for agent to beacon...', 'green')
-        else:
-            self.console.line('No agent found for idx = %s.' % agent_idx, 'red')
