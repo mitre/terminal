f4f9c930da1e9a04bf981fa6ed47102de078f921
Hunt, David <davidhunt@mitre.org>
2019-12-16 10:42:18 -0500

    converting terminal form to an emulation, kind of

--- hook.py
+++ hook.py
@@ -12,12 +12,14 @@ address = '/plugin/terminal/gui'
 
 
 async def enable(services):
+    app = services.get('app_svc').application
     with open('plugins/terminal/conf/terminal_conf.yml', 'r') as fle:
         terminal_config = yaml.safe_load(fle)
     terminal_keys = terminal_config.get('terminal_keys')
     term_svc = TermService(services, terminal_keys)
     await term_svc.start_socket_listener()
-    services.get('app_svc').application.router.add_route('GET', '/plugin/terminal/gui', term_svc.splash)
+    app.router.add_route('GET', '/plugin/terminal/gui', term_svc.splash)
+    app.router.add_static('/terminal', 'plugins/terminal/static/', append_version=True)
 
     await services.get('file_svc').add_special_payload('reverse.go', term_svc.dynamically_compile)
     await services.get('data_svc').load_data(directory='plugins/terminal/data')
