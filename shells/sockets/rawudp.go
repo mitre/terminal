162cd962035b08ee6b28f283bf3764b37422d23b
Hunt, David <davidhunt@mitre.org>
2020-01-30 08:27:14 -0500

    adding a udp response and listening post on manx agent
    Former-commit-id: 37dd624aaaf499124ab16d698f84277a43d9d1a5

--- shells/sockets/rawudp.go
+++ shells/sockets/rawudp.go
@@ -48,7 +48,7 @@ func callMeBack(port int) {
     }
     for {
         _,remoteaddr,err := ser.ReadFromUDP(p)
-        output.VerbosePrint(fmt.Sprintf("[+] instruction received (%v) %s \n", remoteaddr, p))
+        output.VerbosePrint(fmt.Sprintf("[+] instruction received (%v) %s", remoteaddr, p))
         if err !=  nil {
             output.VerbosePrint(fmt.Sprintf("[-] %v", err))
             continue
